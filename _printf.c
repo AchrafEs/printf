c2747353fe9155c22db5fb8bb6ffdd17360c6071
AchrafEs <achraf.es2110@gmail.com>
2023-04-22 15:37:27 +0000

    modified the file _printf.c

--- _printf.c
+++ _printf.c
@@ -16,35 +16,32 @@ int _printf(const char *format, ...)
 	char c;
 
 	va_start(args, format);
-	while (*format)
+	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
 			format++;
-		}
-		if (*format == '\0')
-		{
-			break;
-		}
-		if (*format == 'c')
-		{
-			c = va_arg(args, int);
-			write(1, &c, 1);
-			count++;
-		}
-		else if (*format == 's')
-		{
-			str = va_arg(args, char *);
-			while (*str)
+			switch (*format)
 			{
-				write(1, str++, 1);
-				count++;
+				case 'c':
+					c = va_arg(args, int);
+					write(1, &c, 1);
+					count++;
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					while (*str != '\0')
+					{
+						write(1, str, 1);
+						str++;
+						count++;
+					}
+					break;
+				case '%':
+					write(1, "%", 1);
+					count++;
+					break;
 			}
-		}
-		else if (*format == '%')
-		{
-			write(1, "%", 1);
-			count++;
 		}
 		else
 		{
