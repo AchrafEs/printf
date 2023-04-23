dc6e74f1c1cd97755ab80b0bce8a1cd2cd46da62
AchrafEs <achraf.es2110@gmail.com>
2023-04-23 14:53:07 +0000

    updated the header file

--- main.h
+++ main.h
@@ -1,9 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#include <stdarg.h>
 
 int _printf(const char *format, ...);
-int _printchar(va_list args);
-int _printstr(va_list args);
+void _printchar(char c, int *count);
+void _printstr(char *str, int *count);
 
 #endif
