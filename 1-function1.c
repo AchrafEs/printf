9324e68f7f48bc5c20410963fcca6ce322631716
Yahya Arhoune <yahya.arhoune@gmail.com>
2023-04-24 03:05:51 -0700

    updated

--- 1-function1.c
+++ 1-function1.c
@@ -1,13 +1,10 @@
 #include "main.h"
-#include <limits.h>
 #include <stdio.h>
 
 /**
- * The d and i conversion specifiers are used to output integers.
- *  The d specifier used to print decimal integer.
- *  The i specifier used to print integers in either decimal
- *  or octal or hexadicimal format
- *  just based on the prefix of the output value.
+ * function 1 usint "d" and "i" specifiers
+ *
+ *
  *  Return: always 0.
  */
 
@@ -16,8 +13,8 @@ int main(void)
 	int number1 = 9876;
 	int number2 = -6789;
 
-	printf("number1 using 'd' : %d\n", number1);
-	printf("number2 using 'i' : %i\n", number2);
+	printf("number1 is : %d\n", number1);
+	printf("number2 is : %i\n", number2);
 
 	return (0);
 }
