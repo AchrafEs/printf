a56f2e9ee3e7659cfa841257631878e01a55ce64
Yahya Arhoune <yahya.arhoune@gmail.com>
2023-04-24 05:39:07 -0700

    updated

---
+++ _printf.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _printf - produces output according to a format.
+ * @format: a character string
+ *
+ * Return: the number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				int ch = va_arg(ars, int);
+				putchar(*str);
+				str++;
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				while (*str != '\0')
+				{
+					putchar(*str);
+					str++;
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+
+	va_end(args);
+
+	return count;
+}
+
+int main(void)
+{
+	int count = _printf("Hello, %s! Today is %c.\n", "world", 'Y');
+	printf("Total characters printed is: %d\n", count);
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_integers(int number1, int number2);
+int _printf(const char *format, ...);
+#endif
