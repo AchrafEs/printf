7c57a4000e9b87970e5c83902d9d4f99070773f8
Yahya Arhoune <yahya.arhoune@gmail.com>
2023-04-24 03:15:24 -0700

    updated

--- 1-function1.c
+++ 1-function1.c
@@ -8,13 +8,18 @@
  *  Return: always 0.
  */
 
+void print_integers(int number1, int number2)
+{
+	printf("number1 : %d\n", number1);
+	printf("number2 : %i\n", number2);
+}
+
 int main(void)
 {
 	int number1 = 9876;
 	int number2 = -6789;
 
-	printf("number1 is : %d\n", number1);
-	printf("number2 is : %i\n", number2);
+	print_integers(number1, number2);
 
 	return (0);
 }
