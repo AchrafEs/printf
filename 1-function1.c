c7e13b47df37451e404e3f2b727542b0a3a2d1e4
Yahya Arhoune <yahya.arhoune@gmail.com>
2023-04-24 02:38:20 -0700

    Task1

---
+++ 1-function1.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * The d and i conversion specifiers are used to output integers.
+ *  The d specifier used to print decimal integer.
+ *  The i specifier used to print integers in either decimal
+ *  or octal or hexadicimal format
+ *  just based on the prefix of the output value.
+ *  Return: always 0.
+ */
+
+int main()
+{
+	int number1 = 9876;
+	int number2 = -6789;
+
+	printf("number1 using 'd' : %d\n", number1);
+	printf("number2 using 'i' : %i\n", number2);
+
+	return 0;
+}
