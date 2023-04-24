6b54f7156e2fa07703eb6935367e2a7e73bb697d
AchrafEs <achraf.es2110@gmail.com>
2023-04-24 23:51:17 +0000

    Updated the header file

--- main.h
+++ main.h
@@ -2,8 +2,12 @@
 #define _MAIN_H_
 #include <stddef.h>
 
+#define BUF_SIZE 256
+
 int _printf(const char *format, ...);
-int _putchar(char c);
-int _write_str(char *str, size_t len);
+int _putchar(int c, int *count);
+int print_str(char *str, int *count);
+int print_wchar(wchar_t wc, int *count);
+int print_wstr(wchar_t *ws, int *count);
 
 #endif
