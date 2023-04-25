a9af2e0941a1a6627d7a6f63292c3f3daf820fcb
AchrafEs <achraf.es2110@gmail.com>
2023-04-25 23:28:12 +0000

    updated the header

--- main.h
+++ main.h
@@ -1,13 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #include <stddef.h>
+#include <stdarg.h>
 
-#define BUF_SIZE 1024
+#define BUFF_SIZE 1024
 
 int _printf(const char *format, ...);
-int _putchar(int c, int *count);
-int print_str(char *str, int *count);
-int print_wchar(wchar_t wc, int *count);
-int print_wstr(wchar_t *ws, int *count);
+void print_buffer(char *buffer, size_t size);
+int handle_conversion(const char **format, va_list *args, char *buffer, int *buffer_size);
 
 #endif
